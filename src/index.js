1edb2687f114029c06a5c84fe12b1c4561039eb9
vladcuevas <vladcuevas@gmail.com>
2022-07-05 11:43:39 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// console.log(React.version)
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// const element = document.createElement("h2")
+// element.className = "dummy"
+// element.textContent = "the h2 context"
+
+// document.body.appendChild(element)
+
+// console.dir(element)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+const element = React.createElement("h2")
+
+console.dir(element)
